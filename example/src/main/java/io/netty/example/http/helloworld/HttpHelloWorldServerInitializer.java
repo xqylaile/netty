090e5c732d4ab41f4230216238b03cfd2e22f2ca
xuqy <xuqy_108@163,com>
2025-03-04 17:06:13 +0800

    日常提交

--- example/src/main/java/io/netty/example/http/helloworld/HttpHelloWorldServerInitializer.java
+++ example/src/main/java/io/netty/example/http/helloworld/HttpHelloWorldServerInitializer.java
@@ -23,6 +23,13 @@ import io.netty.handler.codec.http.HttpContentCompressor;
 import io.netty.handler.codec.http.HttpServerCodec;
 import io.netty.handler.codec.http.HttpServerExpectContinueHandler;
 import io.netty.handler.ssl.SslContext;
+/**
+ * @author xuqy
+ * @description //NettyHttp的http请求编码器
+ * @createTime  2025/2/26 10:28 
+ * @param 
+ * @return 
+ **/
 
 public class HttpHelloWorldServerInitializer extends ChannelInitializer<SocketChannel> {
 
@@ -38,6 +45,14 @@ public class HttpHelloWorldServerInitializer extends ChannelInitializer<SocketCh
         if (sslCtx != null) {
             p.addLast(sslCtx.newHandler(ch.alloc()));
         }
+        /**
+         * 调用#new HttpServerCodec()方法，编解码器支持部分 HTTP 请求解析，比如 HTTP GET请求所传递的参数是包含在 uri 中的，
+         * 因此通过 HttpRequest 既能解析出请求参数。
+         *
+         * HttpRequestDecoder 即把 ByteBuf 解码到 HttpRequest 和 HttpContent。
+         * HttpResponseEncoder 即把 HttpResponse 或 HttpContent 编码到 ByteBuf。
+         * HttpServerCodec 即 HttpRequestDecoder 和 HttpResponseEncoder 的结合。
+         **/
         p.addLast(new HttpServerCodec());
         p.addLast(new HttpContentCompressor((CompressionOptions[]) null));
         p.addLast(new HttpServerExpectContinueHandler());
